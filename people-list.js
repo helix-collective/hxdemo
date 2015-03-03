3101add97c33333d40d58de42ac599ffabe40412
Daniel Danilatos <daniel@helixta.com.au>
2015-03-03 20:12:19 +1100

    added stub people list

--- people-list.js
+++ people-list.js
@@ -1,3 +1,76 @@
-module.exports = function PeopleListCtrl(createUi, route, make) {
+var people = [
+  {
+    id: 'aaa',
+    name: 'Jade'
+  },
+  {
+    id: 'bbb',
+    name: 'Dan'
+  },
+  {
+    id: 'ccc',
+    name: 'Jeeva'
+  },
+];
 
-};
+function getPerson(id) {
+  for (var i = 0; i < people.length; i++) {
+    if (people[i].id == id) {
+      return people[i];
+    }
+  }
+  throw new Error('Invalid id: ' + id);
+}
+
+function PersonCtrl(createUi, /*route,*/ person/*, service */) {
+  var me = this;
+  //me.ui = createUi('people.html', {
+  //  loading: true,
+  //  people: null,
+  //  createJob: function() {
+  //    route.go('/jobs/!create:people=' + personId);
+  //  }
+  //});
+
+  //service.get(personId).then(function(person) {
+  //  me.ui.person = person;
+  //  person.defJson = JSON.stringify(person.definition, null, 4);
+  //  console.log(person);
+  //}).finally(function() {
+  //  me.ui.loading = false;
+  //});
+
+
+  me.ui = createUi('person.html', {
+    person: person
+  });
+
+}
+
+
+module.exports = function PeopleListCtrl(createUi, route, make) {
+  var me = this;
+  
+//  var createPeopleRoute = route.child('!create');
+//  var createPeopleCtrl = make(CreatePeopleCtrl, {route:createPeopleRoute});
+
+  route([
+//    [createPeopleRoute.basename(), function(setCurrent) {
+//      setCurrent(createPeopleCtrl);
+//      return createPeopleRoute;
+//    }],
+    [/(.+)/, function(setCurrent, id) {
+      setCurrent(make(PersonCtrl, {
+        person: getPerson(id)
+      }));
+      return route.child(id);
+    }]
+  ]);
+
+  me.ui = createUi('people-list.html', {
+    route: route,
+    items: people,
+    go: route.go,
+  });
+
+}
